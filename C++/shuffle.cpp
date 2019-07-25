75c41a863c20787fad56107e5c37b6b175d8a3f8
matthew-howe <mattehowe@gmail.com>
2019-07-24 20:47:34 -0400

    adds shuffle in C++

---
+++ C++/shuffle.cpp
@@ -0,0 +1,42 @@
+// fisher yates shuffle
+
+size_t getRandom(size_t floor, size_t ceiling)
+{
+	static random_device rdev;
+	static default_random_engine generator(rdev());
+	static uniform_real_distribution<double> distribution(0.0, 1.0);
+	double value = distribution(generator);
+	return static_cast<size_t>(value * (ceiling - floor + 1)) + floor;
+}
+
+void shuffle(vector<int>& theVector)
+{
+	// if it's 1 or 0 items, just return
+	if (theVector.size() <= 1) {
+		return;
+	}
+	for (size_t indexWeAreChoosingFor = 0;
+			indexWeAreChoosingFor < theVector.size() - 1;
+			++indexWeAreChoosingFor)
+	{
+
+		// choose a random not yet placed item to place
+		// it must be an item AFTER the current item,
+		// because the stuff before has all already
+		// been placed !
+		
+		sie_t randomChoiceIndex = getRandom(indexWeAreChoosingFor,
+				theVector.size() - 1);
+
+		// place the random choice in spot with a swap
+		
+		if (randomChoiceIndex != indexWeAreChoosingFor)
+		{
+			swap(theVector[indexWeAreChoosingFor], theVector[randomChoiceIndex])
+		}
+
+	}
+}
+
+// O(n) time
+// O(1) space
